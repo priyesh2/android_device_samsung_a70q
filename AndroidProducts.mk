66eea4bfa190fe1a7b40532543efc8efff4759f1
Lorenzo Mac-Donald <lorenzo.macdonald@icloud.com>
2019-12-24 14:43:52 +0100

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/havoc_a70q.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_a70q-user \
+    havoc_a70q-userdebug \
+    havoc_a70q-eng
---
+++ releasetools.py
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import common
+
+def FullOTA_Assertions(info):
+  AddBootloaderAssertion(info)
+  return
+
+def FullOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+
+def AddBootloaderAssertion(info):
+  cmd = 'assert(getprop("ro.boot.rp") == "3" || abort("ERROR: This package requires bootloeader from version 3 or newer. Please upgrade bootloader and retry!"););'
+  #info.script.AppendExtra(cmd)
+
+def AddImage(info, basename, dest):
+  path = "IMAGES/" + basename
+  if path not in info.input_zip.namelist():
+    return
+
+  data = info.input_zip.read(path)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def OTA_InstallEnd(info):
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
