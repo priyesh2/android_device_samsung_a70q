cd813ad95e5187df57aa4278fe133cf340cc9b10
CEREBRO <33547482+priyesh2@users.noreply.github.com>
2019-12-29 16:39:13 +0530

    fix

--- device.mk
+++ device.mk
@@ -210,6 +210,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+#Keymaster 
+PRODUCT_PACKAGES += \
+ android.hardware.keymaster@4.0-service \
+ android.hardware.keymaster@4.0-impl \
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
